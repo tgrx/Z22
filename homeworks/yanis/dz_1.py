a79d13f6bd6e3219b34259c650c6caf35b55248f
Yanis Romanovich <thrasherdota2@gmail.com>
2019-11-19 21:35:27 +0300

    Lesson 1 version 3

---
+++ homeworks/yanis/dz_1.py
@@ -0,0 +1,21 @@
+import platform
+
+
+def fnc4():
+    name = input("Enter your name")
+    return name
+
+
+def fnc(argument):
+    switcher = {
+        1: "Hello world",
+        2: "My name is Yan. I'm student of BSUIR', 29.07.2000",
+        3: platform.uname(),
+        4: fnc4(),
+    }
+    return switcher.get(argument, "Incorrect value")
+
+
+NOMER_VARIANTA = int(input("Enter number 1-4:"))
+RESULT = fnc(NOMER_VARIANTA)
+print(RESULT)
