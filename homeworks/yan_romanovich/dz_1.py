a1655fd3af24cd62adb81f393e549fc0ba5ff43a
Yanis Romanovich <thrasherdota2@gmail.com>
2019-11-20 20:57:58 +0300

    Happy version

--- homeworks/yan_romanovich/dz_1.py
+++ homeworks/yan_romanovich/dz_1.py
@@ -1,21 +1,20 @@
 import platform
+import typing
 
 
 def fnc4():
     name = input("Enter your name")
-    return name
+    return f"Hello {name} !"
 
 
-def fnc(argument):
-    switcher = {
-        1: "Hello world",
-        2: "My name is Yan. I'm student of BSUIR', 29.07.2000",
-        3: platform.uname(),
-        4: fnc4,
-    }
-    return switcher.get(argument, "Incorrect value")
-
-
-NOMER_VARIANTA = int(input("Enter number 1-4:"))
-RESULT = fnc(NOMER_VARIANTA)
-print(RESULT)
+SWITCHER = {
+    1: "Hello world",
+    2: "My name is Yan. I'm student of BSUIR', 29.07.2000",
+    3: platform.uname(),
+    4: {fnc4},
+}
+RESULT = SWITCHER[int(input("Enter number"))]
+if isinstance(RESULT, typing.Callable):
+    print(RESULT())
+else:
+    print(RESULT)
