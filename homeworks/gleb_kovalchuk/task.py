d93cdf052bca51731f8faf484c800a9e2b9b10e6
glebkov1992 <57806299+glebkov1992@users.noreply.github.com>
2019-11-26 21:21:32 +0200

    firstxxx

---
+++ homeworks/gleb_kovalchuk/task.py
@@ -0,0 +1,5 @@
+def reverse_string(s):
+    return s[::-1]
+
+
+print(reverse_string("1,2,3,4,5"))
---
+++ homeworks/gleb_kovalchuk/test_task.py
@@ -0,0 +1,5 @@
+from homeworks.gleb_kovalchuk.task import reverse_string
+
+
+def test():
+    assert reverse_string("abc") == "cba"
