9f0e03da133d8bdade9aa2a2eb62825414191063
Yanis Romanovich <thrasherdota2@gmail.com>
2019-11-29 19:31:13 +0300

    Homework

--- homeworks/yan_romanovich/hw05/level03.py
+++ homeworks/yan_romanovich/hw05/level03.py
@@ -1,4 +1,4 @@
 def good_phone(nomer):
     if len(nomer) == 13 and nomer[:4:] == "+375":
-        return "good_phone"
-    return "not good_phone"
+        return True
+    return False
--- homeworks/yan_romanovich/hw05/test_lvl03.py
+++ homeworks/yan_romanovich/hw05/test_lvl03.py
@@ -2,4 +2,7 @@ from homeworks.yan_romanovich.hw05 import level03
 
 
 def test():
-    assert level03.good_phone("+375293654313") == "good_phone"
+    assert level03.good_phone("+375172294660")
+    assert level03.good_phone("+375296648778")
+    assert not level03.good_phone("+791562655123")
+    assert not level03.good_phone("2020327")
