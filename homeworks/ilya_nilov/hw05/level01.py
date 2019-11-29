139204557a4ea918cc3a86292c386a90c1173444
Ilya <hacharoky86@gmail.com>
2019-11-29 18:57:46 +0300

    chnges

--- homeworks/ilya_nilov/hw05/level01.py
+++ homeworks/ilya_nilov/hw05/level01.py
@@ -1,7 +1,7 @@
 def summa (nmbr1, nmbr2):
     try:
-        summ = nmbr1 + nmbr2
+        nmbr1 =  __add__(self, nmbr2)
     except ValueError:
         return("Неверное значение")
-    return(summ)
+    return(nmbr1)
       
--- homeworks/ilya_nilov/hw05/test_lev1.py
+++ homeworks/ilya_nilov/hw05/test_lev1.py
@@ -1,6 +1,6 @@
-from homeworks.ilya_nilov.hw05 import level01
+from homeworks.ilya_nilov.hw05.level01 import summa
 
 
 def test():
-    assert level01.summa(3,5) == 8
+    assert summa("3","5") == "35"
 
