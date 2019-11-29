8842a1143d9ca621d5821ea5c560d358b2ed6b55
Yanis Romanovich <thrasherdota2@gmail.com>
2019-11-29 21:32:02 +0300

    asdfh

--- homeworks/yan_romanovich/hw05/level03.py
+++ homeworks/yan_romanovich/hw05/level03.py
@@ -1,4 +1,6 @@
 def good_phone(nomer):
-    if len(nomer) == 13 and "+375" in nomer and nomer[1:].isdigit():
-        return True
+    if nomer != None:
+        if len(nomer) == 13 and "+375" in nomer and nomer[1:].isdigit():
+            return True
+        return False
     return False
