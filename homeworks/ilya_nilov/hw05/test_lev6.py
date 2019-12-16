27fdb05ad8660967a9aa9615fef50c65ebb5aa99
Ilya <hacharoky86@gmail.com>
2019-12-16 11:09:57 +0300

    lvlvlv2

--- homeworks/ilya_nilov/hw05/test_lev6.py
+++ homeworks/ilya_nilov/hw05/test_lev6.py
@@ -3,21 +3,21 @@ from homeworks.ilya_nilov.hw05.level06 import dequeue
 
 
 def test():
-    q = []
+    q_lits = []
 
-    assert enqueue(q, 100) is None
-    assert enqueue(q, 200) is None
-    assert len(q) == 2
+    assert enqueue(q_lits, 100) is None
+    assert enqueue(q_lits, 200) is None
+    assert len(q_lits) == 2
 
-    assert dequeue(q) == 100
-    assert len(q) == 1
+    assert dequeue(q_lits) == 100
+    assert len(q_lits) == 1
 
-    assert enqueue(q, 300) is None
-    assert len(q) == 2
+    assert enqueue(q_lits, 300) is None
+    assert len(q_lits) == 2
 
-    assert dequeue(q) == 200
-    assert dequeue(q) == 300
-    assert len(q) == 0
+    assert dequeue(q_lits) == 200
+    assert dequeue(q_lits) == 300
+    assert len(q_lits) == 0
 
-    assert dequeue(q) is None
-    assert len(q) == 0
+    assert dequeue(q_lits) is None
+    assert len(q_lits) == 0
