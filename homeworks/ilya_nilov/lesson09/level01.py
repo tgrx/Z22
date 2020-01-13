a4a83f48733c8bf6bb531dc92fb07423c240dede
Ilya <hacharoky86@gmail.com>
2020-01-13 20:37:53 +0300

    deleted

--- homeworks/ilya_nilov/lesson09/level01.py
+++
@@ -1,9 +0,0 @@
-def compare_triplets(tr1, tr2):
-    while i <= tr1:
-        if tr1[i] > tr2[i]:
-            cntr[0] = +1
-        if tr1[i] == tr2[i]:
-            cntr[1] = -1
-        cntr[1] = +1
-        i = +1
-    return cntr
--- homeworks/ilya_nilov/lesson09/level02.py
+++
@@ -1,4 +0,0 @@
-def rotate_left(lits, nrot):
-    cplits = lits[nrot:]
-    cplits.extend(lits[:nrot])
-    return cplits
