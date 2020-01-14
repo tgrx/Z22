8c0d235e2716c8e2686dc3bfe0a1978b0df585f7
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-01-14 21:48:39 +0300

    added tests for big_summa

--- lessons/lesson10/tests/test_level01.py
+++ lessons/lesson10/tests/test_level01.py
@@ -10,6 +10,8 @@ def verify(module):
     assert isinstance(big_summa, Callable)
 
     assert big_summa(i for i in range(10)) == 45
+    assert big_summa((i for i in range(10)), 1) == 46
+    assert big_summa(2, (i for i in range(10)), 1) == 48
     assert big_summa({-1, 1, 0}) == 0
 
 
