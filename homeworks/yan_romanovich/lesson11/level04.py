165f9ddd618d612038befd8f9fc7b8d46db7ecde
Yan <40761016+rko619619@users.noreply.github.com>
2020-01-28 20:57:38 +0300

    Yan romanovich less11 (#85)
    * task 1,2,3
    
    *  fixed task 1,2,3
    
    * fixed
    
    * clean version task4.0
    
    * clean version task4.0
    
    * clean version task4.1
    
    * Task05
    
    * fixed task05
    
    * bad
    
    * bad 1.0:Q
    
    * fixed task04
    
    * fixed
    
    * fixed
    
    * fixed

--- homeworks/yan_romanovich/lesson11/level04.py
+++ homeworks/yan_romanovich/lesson11/level04.py
@@ -3,12 +3,21 @@ from homeworks.yan_romanovich.lesson11 import level03
 
 class User(level03.User):
     def validate(self):
+        if not self.email:
+            raise ValueError
         elm = str(self.email).index("@")
         lst1 = str(self.email[:elm])
+        if not str(self.email[elm + 1 :]):
+            raise ValueError
         lst2 = str(self.email[elm + 1 :])
         if not (self.name and lst1):
             raise ValueError
-        if not (self.name.isalnum() and lst1.isalnum()):
+        if not (
+            self.name.isalnum()
+            and "a" <= self.name <= "z"
+            and lst1.isalnum()
+            and "a" <= lst1 <= "z"
+        ):
             raise ValueError
         if not (self.name[0].isalpha() and lst1[0].isalpha()):
             raise ValueError
