86995b5a95e34b762e40ccc088e44c418072eb80
HusainovaAI <55806249+HusainovaAI@users.noreply.github.com>
2020-01-29 20:39:57 +0300

    Delete level01.py

--- homeworks/anastasia_husainova/lesson05/level01.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Enter an integer: "))
-y = int(input("Enter another integer: "))
-summa = x+y
-print("summa (", x, ' , ', y, ') == ', summa, '.', sep='')
-
-
-# def summa(x,y):
-#     return x+y
