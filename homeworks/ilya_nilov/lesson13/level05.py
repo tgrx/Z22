a142b20417b8c446f8d67a70ba0c113869b6869d
Ilya <hacharoky86@gmail.com>
2020-02-10 21:19:15 +0300

    lvl5

---
+++ homeworks/ilya_nilov/lesson13/level05.py
@@ -0,0 +1,12 @@
+class Server:
+    def __init__(self, host: str):
+        self.host = host
+
+    def add_handler(path: str, handler: function):
+        pass
+
+    def serve(url) -> str:
+        pass
+
+    class Redirect(Exception):
+        pass
