4ce0a9d5abf566389add9c40b3652ca99b230618
kirill-shevchuk17 <kirill-shevchuk1@mail.ru>
2020-02-11 21:00:42 +0300

    lvl 7

--- homeworks/kirill_shevchuk/lesson17/level07.py
+++ homeworks/kirill_shevchuk/lesson17/level07.py
@@ -1,4 +1,12 @@
-def typecheck(function):
-    def decorator():
-        print(get_type_hints())
+def typecheck(function, *ar, **kw):
+    def decorator(*ar, **kw):
+        type_hints = function.__annotations__
+        for key, value in type_hints:
+            if key == "return":
+                result = function(*ar, **kw)
+                if not isinstance(result, value):
+                    raise TypeError
+            elif not isinstance(key, value):
+                raise TypeError
+        return result
     return decorator
