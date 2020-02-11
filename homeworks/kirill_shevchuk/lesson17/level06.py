e076260e46b1e56f2068df6019b2d31db93d284b
kirill-shevchuk17 <kirill-shevchuk1@mail.ru>
2020-02-11 20:17:58 +0300

    lvl 6

--- homeworks/kirill_shevchuk/lesson17/level06.py
+++ homeworks/kirill_shevchuk/lesson17/level06.py
@@ -3,8 +3,11 @@ import time
 
 def benchmark(function, *ar, **kw):
     def decorator(*ar, **kw):
-        start_time = time.time()
-        function(*ar, **kw)
-        print(int(time.time() - start_time))
+        try:
+            start_time = time.time()
+            function(*ar, **kw)
+            print(int(time.time() - start_time))
+        except Exception:
+            print(0)
         return function(*ar, **kw)
     return decorator
