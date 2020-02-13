f3ca8547a9ebc1352427ebe1b36fe2398b5ada97
kirill-shevchuk17 <kirill-shevchuk1@mail.ru>
2020-02-13 14:47:27 +0300

    lvl 8

---
+++ homeworks/kirill_shevchuk/lesson17/level08.py
@@ -0,0 +1,7 @@
+class Functor:
+    def __init__(self, function, *ar, **kw):
+        self.function = function
+        self.ar = ar
+        self.kw = kw
+    def __call__(self):
+        return self.function(self.ar, self.kw)
