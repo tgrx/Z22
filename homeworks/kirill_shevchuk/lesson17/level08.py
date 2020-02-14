595313e1de8cec95730f0cf26566430cb2a2f298
kirill-shevchuk17 <kirill-shevchuk1@mail.ru>
2020-02-14 19:45:14 +0300

    Correct tasks

--- homeworks/kirill_shevchuk/lesson17/level08.py
+++ homeworks/kirill_shevchuk/lesson17/level08.py
@@ -1,7 +1,7 @@
 class Functor:
-    def __init__(self, function, ar, kw):
+    def __init__(self, function, ar=None, kw=None):
         self.function = function
-        self.ar = ar
-        self.kw = kw
+        self.ar = ar or ()
+        self.kw = kw or {}
     def __call__(self):
         return self.function(*self.ar, **self.kw)
