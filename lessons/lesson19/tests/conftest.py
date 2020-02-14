c0e4c0c346d3c21e718d69b723ef31804fc1a1f7
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-02-14 18:08:27 +0300

    Added task & test for lesson 19 (#112)

---
+++ lessons/lesson19/tests/conftest.py
@@ -0,0 +1,24 @@
+from typing import Any
+from typing import Dict
+from typing import Text
+
+import pytest
+
+from project.settings import HOMEWORKS
+from project.utils import import_by_path
+
+
+def find_modules_for_level(level: Text) -> Dict[Text, Any]:
+    modules = {}
+
+    for pyfile in HOMEWORKS.glob(f"**/lesson19/{level}.py"):
+        student = pyfile.parts[-3]
+        module = import_by_path(pyfile)
+        modules[student] = module
+
+    return modules
+
+
+@pytest.fixture
+def modules_level01() -> Dict[Text, Any]:
+    return find_modules_for_level("level01")
